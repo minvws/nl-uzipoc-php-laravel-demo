0e06b8c867516e8e04380c9dad16e00529d3c677
Rick Lambrechts <rick@rl-webdiensten.nl>
2024-01-31 12:34:34 +0100

    Add sidebar to no relations page (#103)

--- resources/views/ziekenboeg/auth/no-relations.blade.php
+++ resources/views/ziekenboeg/auth/no-relations.blade.php
@@ -1,8 +1,39 @@
-@extends('layouts.ziekenboeg.app')
+@extends('layouts.ziekenboeg.app-with-sidebar')
 
 @php
 /* @var $user \App\Models\UziUser */
 @endphp
+
+@section('sidemenu')
+    <nav class="ziekenboeg-sidemenu">
+        <div>
+            <div class="sticky-top">
+                <h3>Identiteit heeft geen toegang tot de Ziekenboeg</h3>
+
+                <p>
+                    Enkele (test)identiteiten hebben geen toegang tot de Ziekenboeg; de identiteit in het proeftuin UZI-register bevat geen URA-relatie met de Ziekenboeg. Zo kunnen ook incorrecte inlogpogingen getest worden.
+                </p>
+
+                <p>
+                    Correcte inlogpogingen kunnen worden getest met een andere (test)identiteit.
+                </p>
+
+                <p>
+                    Klik op <b>“Terug naar inloggen”</b> om verder te gaan.
+                </p>
+            </div>
+            <div class="sticky-bottom">
+                <ul>
+                    <li>
+                        <a href="{{ route('index') }}" class="back-to-proeftuin">
+                            Terug naar de Proeftuin hoofdpagina
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+@endsection
 
 @section('content')
     <section>
